66dc94d4cd8519680f161350b7ca766310741ab4
rylearn <40655878+rylearn@users.noreply.github.com>
2018-09-04 23:40:18 -0500

    Editing comments

--- Valarray.h
+++ Valarray.h
@@ -1,16 +1,5 @@
 // Valarray.h
 
-/* Put your solution in this file, we expect to be able to use
- * your epl::valarray class by simply saying #include "Valarray.h"
- *
- * We will #include "Vector.h" to get the epl::vector<T> class
- * before we #include "Valarray.h". You are encouraged to test
- * and develop your class using std::vector<T> as the base class
- * for your epl::valarray<T>
- * you are required to submit your project with epl::vector<T>
- * as the base class for your epl::valarray<T>
- */
-
 #ifndef _Valarray_h
 #define _Valarray_h
 
@@ -338,7 +327,6 @@ public:
             pref = ", ";
             k++;
         }
-        print(out); // SJ not sure
     }
 };
 
@@ -378,7 +366,6 @@ public:
             pref = ", ";
             k++;
         }
-        print(out); // SJ not sure
     }
 };
 
